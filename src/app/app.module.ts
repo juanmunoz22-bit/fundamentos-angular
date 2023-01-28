49bc8d9faf0fe55996d0bef0b91ab3da4e841c86
Juan <juanpablo.munoz@uber.com>
2023-01-27 21:03:33 -0500

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
   ],
   imports: [
     BrowserModule,
